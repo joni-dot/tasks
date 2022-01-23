1f984f1fba21ca5eaf41603091fa102b642b01f8
Joni <minjonlu@gmail.com>
2022-01-23 12:57:09 +0200

    CreateUser command to create users

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create user and save user to the database.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::create([
+            'name' => $this->ask('What is the name?'),
+            'email' => $this->ask('What is the email?'),
+            'password' => bcrypt($this->secret('What is the password?')),
+        ]);
+    }
+}
