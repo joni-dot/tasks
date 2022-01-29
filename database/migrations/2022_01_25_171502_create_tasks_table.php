fde10ea2e2b0e34c12194b78f2cdb48ef076cb71
Joni <minjonlu@gmail.com>
2022-01-29 12:32:01 +0200

    Task model, factory and seeder for test data

--- database/migrations/2022_01_25_171502_create_tasks_table.php
+++ database/migrations/2022_01_25_171502_create_tasks_table.php
@@ -16,7 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
