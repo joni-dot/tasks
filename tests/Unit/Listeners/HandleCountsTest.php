e1ef0f32272d9a3f783271a25308f3e2dec93cc4
Joni <minjonlu@gmail.com>
2022-03-20 21:07:20 +0200

    Test that count also decrease

--- tests/Unit/Listeners/HandleCountsTest.php
+++ tests/Unit/Listeners/HandleCountsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Listeners;
 
 use App\Actions\App\Tasks\CreateTask;
+use App\Actions\App\Tasks\DeleteTask;
 use App\Models\Count;
 use App\Models\Task;
 use Tests\TestCase;
@@ -29,6 +30,24 @@ class HandleCountsTest extends TestCase
         $this->assertDatabaseHas((new Count)->getTable(), [
             'countable_type' => (new Task)->getMorphClass(),
             'count' => 1,
+        ]);
+    }
+
+    /** @test */
+    public function it_counts_when_decreased()
+    {
+        Count::create([
+            'countable_type' => (new Task)->getMorphClass(),
+            'count' => 10,
+        ]);
+
+        (new DeleteTask)->delete(
+            Task::factory()->create()
+        );
+
+        $this->assertDatabaseHas((new Count)->getTable(), [
+            'countable_type' => (new Task)->getMorphClass(),
+            'count' => 9,
         ]);
     }
 }
